ec2cb59e7ad038086209b5fe86e67e385c808e78
christian@voegtle.org <christian@voegtle.org>
2026-09-07 21:30:43 +0200

    Umstellung auf Maven Build

--- deploy.sh
+++ deploy.sh
@@ -8,13 +8,7 @@ PROJECT="${PROJECT:-spieleclub-paderborn-test}"
 
 export JAVA_HOME="${JAVA_HOME:-/Library/Java/JavaVirtualMachines/temurin-21.jdk/Contents/Home}"
 
-echo "Building GWT client modules..."
-CP_FILE="$(mktemp)"
-/Users/cv/Library/apache-maven-3.9.9/bin/mvn -q dependency:build-classpath -Dmdep.outputFile="$CP_FILE"
-java -cp "src:$(cat "$CP_FILE")" com.google.gwt.dev.Compiler -war web -style OBFUSCATED -sourceLevel 17 de.spieleclub.spieleclub_capture de.spieleclub.spieleclub_analysis
-rm -f "$CP_FILE"
-
-echo "Building WAR with Maven on Java 21..."
+echo "Building application with Maven (including GWT) on Java 21..."
 /Users/cv/Library/apache-maven-3.9.9/bin/mvn clean package
 
 echo "Deploying to App Engine project: $PROJECT..."
